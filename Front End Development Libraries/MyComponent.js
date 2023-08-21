f4eb517444dacb5470ae406c5fbf2791bd724cb0
YGnina <93010504+YGnina@users.noreply.github.com>
2023-08-20 21:55:07 -0400

    1st React Component

--- Front End Development Libraries/MyComponent.js
+++ Front End Development Libraries/MyComponent.js
@@ -1,17 +1,15 @@
-class TypesOfFood extends React.Component {
+// Change code below this line
+class MyComponent extends React.Component{
   constructor(props) {
     super(props);
   }
-  render() {
-    return (
-      <div>
-        <h1>Types of Food:</h1>
-        {/* Change code below this line */}
 
-        {/* Change code above this line */}
-      </div>
+  render(){
+    return(
+      <div><h1>My First React Component!</h1></div>
     );
   }
+
 };
 
-// Change code below this line
+ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
