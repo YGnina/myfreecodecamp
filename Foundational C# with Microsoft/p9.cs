eaba84626c4ecbe5ab7e8d29c6f969c8a76c2db1
YGnina <93010504+YGnina@users.noreply.github.com>
2023-09-02 19:10:35 -0400

    data type

---
+++ Foundational C# with Microsoft/p9.cs
@@ -0,0 +1,12 @@
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+// Signed integral types:
+// sbyte  : -128 to 127
+// short  : -32768 to 32767
+// int    : -2147483648 to 2147483647
+// long   : -9223372036854775808 to 9223372036854775807
