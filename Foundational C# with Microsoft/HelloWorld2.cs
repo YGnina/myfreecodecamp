8ed2406f3c757059d43dc1e9779ef53599965e92
YGnina <93010504+YGnina@users.noreply.github.com>
2023-09-02 09:46:22 -0400

    Multi-language HelloWorld

--- Foundational C# with Microsoft/HelloWorld2.cs
+++ Foundational C# with Microsoft/HelloWorld2.cs
@@ -2,16 +2,17 @@ Console.WriteLine("Hello\nWorld!");
 
 Console.WriteLine("Hello \"World\"!");
 
-// Unicode escape characters (UTF-16)
+string world = "World";
 
 // こんにちは World!
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+Console.WriteLine($"\u3053\u3093\u306B\u3061\u306F {world}!");
 
 //你好 World!
-Console.WriteLine("\u4F60\u597D World!");
+Console.WriteLine($"\u4F60\u597D {world}!");
 
 //안녕하십스요 World!
-Console.WriteLine("\uC548\uB155\uD558\uC2ED\uC2A4\uC694 World!");
+Console.WriteLine($"\uC548\uB155\uD558\uC2ED\uC2A4\uC694 {world}!");
 
 //שאלום World!
-Console.WriteLine("\u05E9\u05D0\u05DC\u05D5\u05DD World!");
+Console.WriteLine($"\u05E9\u05D0\u05DC\u05D5\u05DD {world}!");
+
