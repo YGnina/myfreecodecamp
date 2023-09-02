fcff0e3697c064692c3a9ae2d27e7673262a106a
YGnina <93010504+YGnina@users.noreply.github.com>
2023-09-02 19:11:46 -0400

    data type

--- Foundational C# with Microsoft/p9.cs
+++ Foundational C# with Microsoft/p9.cs
@@ -10,3 +10,17 @@ Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
 // short  : -32768 to 32767
 // int    : -2147483648 to 2147483647
 // long   : -9223372036854775808 to 9223372036854775807
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Unsigned integral types:
+// byte   : 0 to 255
+// ushort : 0 to 65535
+// uint   : 0 to 4294967295
+// ulong  : 0 to 18446744073709551615
