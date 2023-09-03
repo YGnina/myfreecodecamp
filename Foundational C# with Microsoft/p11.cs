9b4ee60411bdbc055899cedb125f2ed718fc9ab3
YGnina <93010504+YGnina@users.noreply.github.com>
2023-09-03 12:14:17 -0400

    string format

---
+++ Foundational C# with Microsoft/p11.cs
@@ -0,0 +1,38 @@
+// :C format currency
+// :P format percentages
+// :Nx format numbers, display x digits 
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+
+// Invoice Number: 1201
+//    Shares: 25.457 Product
+//      Sub Total: $2,750.00
+//            Tax: 15.83%
+//      Total Billed: $3,185.19
+
+
+
+// The PadLeft() and PadRight() methods add white space (or optionally, another character) to the total length of a string.
+string paymentId = "769";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+
+// 1234567890123456789012345678901234567890
+// 769   Mr. Stephen Ortega       $5,000.00
